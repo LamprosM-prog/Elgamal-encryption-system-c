267f8e7fe2f130c673609096b568c43feff08830
Lampros <lamprosmala@gmail.com>
2026-05-18 20:38:54 +0300

    Add encrypt & decrypt

--- src/elgamal.c
+++ src/elgamal.c
@@ -40,7 +40,7 @@ void generate_pkey(mpz_t result, mpz_t g, mpz_t x, mpz_t p){
     mpz_powm_sec(result, g, x, p);
 }
 
-void encrypt(ElgamalCiphertext* result, mpz_t msg, mpz_t pkey, mpz_t p, mpz_t g){
+void encrypt(ElgamalCiphertext* result, mpz_t msg, mpz_t pkey, ElGamalParam* params){
     gmp_randstate_t state;
     gmp_randinit_mt(state);
     gmp_randseed_ui(state, time(NULL));
@@ -48,8 +48,43 @@ void encrypt(ElgamalCiphertext* result, mpz_t msg, mpz_t pkey, mpz_t p, mpz_t g)
     mpz_init(range);  
     mpz_t k;
     mpz_init(k);
+    mpz_t temp;
+    mpz_init(temp);
+
+    // random k in (1, q-1)
+    mpz_sub_ui(range,params->q,1);
+    mpz_urandomm(k, state, range);
+    mpz_add_ui(k,k,1);
+    
+    //c1 = g^k mod p
+    mpz_powm_sec(result->c1 ,params->g,k,params->p);
+
+    //c2 = msg * pkey^k mod p
+    mpz_powm_sec(temp, pkey, k,params->p); // temp = pkey^k
+    mpz_mul(result->c2,temp,msg); // c2 = msg * temp
+    mpz_mod(result->c2, result->c2, params->p);
+
+    mpz_clear(range);
+    gmp_randclear(state);
+    mpz_clear(k);
+    mpz_clear(temp);
+}
+void decrypt(mpz_t result, ElgamalCiphertext* ct, mpz_t skey, mpz_t p){ //Elgamal!
+    mpz_t s;
+    mpz_init(s);
+    mpz_t s_inv;
+    mpz_init(s_inv);
+
+    //s := c1 ^ x (mod p) , c1 = g^k (mod p) => c1^x = g^k*x (mod p) , g^k*x (modp) = h^x (same as encrypt) 
+    mpz_powm_sec(s, ct->c1, skey, p);
+
+    mpz_invert(s_inv, s, p);
+
+    // m := c2 * s^-1 (mod p)
+    mpz_mul(result, ct->c2,  s_inv);
+    mpz_mod(result, result, p);
 
 
-    mpz_sub_ui(range,?,1);
-
+    mpz_clear(s);
+    mpz_clear(s_inv);
 }
